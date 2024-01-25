c199df5d8dcf03bc116111e3cdef7d0cbaaebf34
Gavy Lau <gavy.lau@privetechnologies.com>
2024-01-25 10:58:23 +0800

    Fix config

--- src/main/java/com/github/rawsanj/config/WebSocketConfig.java
+++ src/main/java/com/github/rawsanj/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/redis-chat").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint("/redis-chat").setAllowedOriginPatterns(".*").withSockJS();
 	}
 
 	@Override
